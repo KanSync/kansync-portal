af000f48e3b19756ebd7caff1a166d579e00258c
Amer Slaiman <91747941+amermsl92@users.noreply.github.com>
2023-11-30 18:32:52 +0100

    initial creation of due-date functions
    showIssuesWithinDaysThreshold() function returns several issues with a deadline within the specified days.
    showClosestDeadlines() function returns a list of issues closest to their deadline.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,40 @@ import Burndown from "./charts/burndown";
 import "./App.css";
 import conv_to_unified from "./utils/parse";
 import { tmp } from "./charts/tmp";
+import {
+  showIssuesWithinDaysThreshold,
+  showClosestDeadlines,
+} from "./utils/dueDataFuntions";
 
 function App() {
-  let parsed_data = conv_to_unified(tmp)
+  let parsed_data = conv_to_unified(tmp);
+
+  const issuesWithinDaysCount = showIssuesWithinDaysThreshold(
+    parsed_data.issues,
+    7,
+  );
+
+  const closestDeadlinesIssues = showClosestDeadlines(parsed_data.issues, 3);
+
   return (
-    <Burndown
-      issues={parsed_data.issues}
-      numIssues={parsed_data.num}
-      endDate={new Date()}
-    />
+    <>
+      <Burndown
+        issues={parsed_data.issues}
+        numIssues={parsed_data.num}
+        endDate={new Date()}
+      />
+      <p>{`Number of issues within 7 days: ${issuesWithinDaysCount}`}</p>
+      <div>
+        <p>Issues closest to their deadline:</p>
+        <ul>
+          {closestDeadlinesIssues.map((issue, index) => (
+            <li
+              key={index}
+            >{`Issue: ${issue.title}, Due Date: ${issue.dueDate}`}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
