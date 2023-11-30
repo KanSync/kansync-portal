7b07a902f76b113668383b0d9359ce13988e27eb
Felix Thörnblad <Felix.Thornblad.9504@student.uu.se>
2023-11-30 21:47:52 +0100

    added DefaultInputProps child to parent communication

--- src/card.tsx
+++ src/card.tsx
@@ -1,32 +1,56 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, ChangeEvent } from "react";
 import PlusCircle from "./assets/plus-circle.svg";
 import { useProject } from "./providers/ProjectProvider";
 
-const DefaultInput = ({ placeholder }: { placeholder?: string }) => {
+
+
+interface DefaultInputProps {
+  placeholder?: string;
+  onChildValueChange: (value: string) => void; // Function prop to send data to the parent
+}
+
+const DefaultInput = ({ placeholder, onChildValueChange }: DefaultInputProps) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    onChildValueChange(e.target.value);
+  };
+
   return (
     <>
       <input
         type="text"
         placeholder={placeholder}
+        value={inputValue}
+        onChange={handleInputChange}
         className="w-full bg-transparent rounded-md border border-stroke dark:border-dark-3 py-[10px] px-5 text-dark-6 outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-gray-2 disabled:border-gray-2"
       />
     </>
   );
 };
 
-const AddCard = () => {
+const AddCard: React.FC = () => {
+  const [receivedValue, setReceivedValue] = useState<string>("");
+
+  // Function to receive data from the child component
+  const handleChildValueChange = (value: string) => {   
+    setReceivedValue(value);
+  };
+
+
   const addProject = useProject().addProject;
 
   const handleClick = useCallback(() => {
     addProject({
-      name: "test",
+      name: receivedValue//"test",
     });
   }, [addProject]);
 
   return (
     <div className="bg-secondary flex flex-row justify-between px-8 center items-center ml-8 py-4">
       <div className="flex flex-row items-center gap-8">
-        <DefaultInput placeholder="Enter name of repository" />
+        <DefaultInput placeholder="Enter name of repository" onChildValueChange={handleChildValueChange}/>
         <a className="hover:bg-accent" onClick={handleClick}>
           <img src={PlusCircle} className="w-16" />
         </a>
