1718881d19f8121aa0166cc97db88cdeb12ae9a8
gw <55237236+gabrielfrimodig@users.noreply.github.com>
2023-12-04 10:23:11 +0100

    Feature/due date (#14)
    * initial creation of due-date functions
    
    showIssuesWithinDaysThreshold() function returns several issues with a deadline within the specified days.
    showClosestDeadlines() function returns a list of issues closest to their deadline.
    
    * CloseToDueDateChart
    
    * working edition
    
    ---------
    
    Co-authored-by: Amer Slaiman <91747941+amermsl92@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import Burndown from "./charts/burndown";
+import React, { useState } from 'react';
 import AverageAge from "./charts/average_age";
 import "./App.css";
 import conv_to_unified from "./utils/parse";
 import { tmp } from "./charts/tmp";
+import CloseToDueDateChart from "./utils/CloseToDueDateChart";
 
 function App() {
+  const [daysThreshold, setDaysThreshold] = useState(7);
   let parsed_data = conv_to_unified(tmp);
-  return <AverageAge issues={parsed_data.issues} />;
+
+  const handleDaysThresholdChange = (event) => {
+    setDaysThreshold(Number(event.target.value));
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="daysThreshold">Days for 'Close to Due Date': </label>
+        <input
+          id="daysThreshold"
+          type="number"
+          value={daysThreshold}
+          onChange={handleDaysThresholdChange}
+        />
+      </div>
+      
+      <CloseToDueDateChart issues={parsed_data.issues} daysThreshold={daysThreshold} />
+    </>
+  );
+ 
+  // return <AverageAge issues={parsed_data.issues} />;
 }
 
 export default App;
