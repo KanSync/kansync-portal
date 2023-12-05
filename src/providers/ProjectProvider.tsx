23130022bbbbd1ff88e2ff71b5125a67d1a5f2bf
Richard Kasendu <kasendu@gmail.com>
2023-12-05 17:17:02 +0100

    Fixes #26. Worked on design part of the dashboard.

--- src/providers/ProjectProvider.tsx
+++ src/providers/ProjectProvider.tsx
@@ -7,22 +7,54 @@ interface IProject {
 }
 
 interface IProjectProvider {
-  activeProjects: IProject[];
+  activeProjects: {
+    Github: IProject[];
+    Jira: IProject[];
+    Trello: IProject[];
+  };
   addProject: (project: IProject) => void;
 }
 
 const ProjectProviderContext = React.createContext<IProjectProvider>({
-  activeProjects: [],
+  activeProjects: {
+    Github: [],
+    Jira: [],
+    Trello: [],
+  },
   addProject: () => {
     throw new Error(`[ProjectProivder] addProject() not implemented`);
   },
 });
 
 const ProjectProvider = ({ children }: { children: React.ReactNode }) => {
-  const [activeProjects, setActiveProjects] = useState<IProject[]>([]);
+  const [activeProjects, setActiveProjects] = useState<{
+    Github: IProject[];
+    Jira: IProject[];
+    Trello: IProject[];
+  }>({
+    Github: [],
+    Jira: [],
+    Trello: [],
+  });
 
   const addProject = (project: IProject) => {
-    setActiveProjects([...activeProjects, project]);
+    switch (project.platform) {
+      case "Github":
+        return setActiveProjects({
+          ...activeProjects,
+          Github: [...activeProjects.Github, project],
+        });
+      case "Jira":
+        return setActiveProjects({
+          ...activeProjects,
+          Jira: [...activeProjects.Jira, project],
+        });
+      case "Trello":
+        return setActiveProjects({
+          ...activeProjects,
+          Trello: [...activeProjects.Trello, project],
+        });
+    }
   };
 
   return (
