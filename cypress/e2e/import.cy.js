857878b78d07b555dbb473a88f3c56ae576dbbfe
Kax-y <56028244+Kax-y@users.noreply.github.com>
2023-12-20 18:02:32 +0100

    Added import and show board test

--- cypress/e2e/import.cy.js
+++ cypress/e2e/import.cy.js
@@ -1,17 +1,24 @@
 describe('import and show board', () => {
+    let token = Cypress.env("githubToken")
     before(() => {
-      window.localStorage.setItem('Github', JSON.stringify({accessToken: Cypress.env('githubToken'), expiresAt: null, refreshToken: null}))
+
+      window.localStorage.setItem('Github', JSON.stringify({accessToken: token, expiresAt: null, refreshToken: null}))
     })
 
     beforeEach(() => {
-      window.localStorage.setItem('Github', JSON.stringify({accessToken: Cypress.env('githubToken'), expiresAt: null, refreshToken: null}))
       cy.visit('/')
     })
   
-    it('import KanSync project', () => {
+    it('import KanSync project and check its board', () => {
+      cy.intercept('GET', 'https://local.functions.nhost.run/v1/github/?repo=*&projectName=*').as('importGithub')
+
       cy.contains('Dashboard').click()
-      cy.get('input[placeholder*="Enter repository"]').type('KanSync/KanSync-server')
+      cy.get('input[placeholder*="Enter repository"]').type('KanSync/kansync-server')
       cy.get('input[placeholder*="Enter name of project"]').type('KanSync Project Planner')
       cy.get('.importButton').click()
+      cy.wait('@importGithub', {responseTimeout: 15000})
+      cy.get('[type="checkbox"]').check()
+      cy.contains('Continue with selected boards').click()
+      cy.contains('Kanban Board').click()
     })
 })
